c309cb8c1a6d7fc8127d44072797d5118a2ad879
Arcani-Dominus <arcanidominus@gmail.com>
2025-03-31 07:20:46 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,7 +3,6 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-firestore.js";
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
